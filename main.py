51ef5b8e2d0752159adb46ebc4830bbc9fad6653
raihan <rahmanraihanaulia@gmail.com>
2025-06-24 19:48:45 +0700

    ignore get_images duplicate using bbox

--- main.py
+++ main.py
@@ -71,7 +71,6 @@ def extract_pdf(doc: pymupdf.Document,start=0, end=-1):
     end = doc.page_count if end == -1 else end
     content = ""
     images = {} # key: filename, value: image data
-    ignored_images = {}
 
     for i in range(start, end):
         cprint.blue(f"{'-'*8}PAGE {i+1}{'-'*8}")
@@ -81,6 +80,7 @@ def extract_pdf(doc: pymupdf.Document,start=0, end=-1):
         img_count = 0
         img_list = page.get_images()
         get_images_count = len(img_list)
+        ignored_images = set()
 
         for element in dicts["blocks"]:
             if element["type"] == 0: # text block
@@ -95,13 +95,12 @@ def extract_pdf(doc: pymupdf.Document,start=0, end=-1):
                 img_bbox = element["bbox"]
                 # img_height =  img_bbox[3] - img_bbox[1]
                 img_filename = f"page_{i+1}-image_{img_count}.png"
-                ignored = False
 
                 # ignores images in headers and bottom part of the page (use threshold)
                 if (img_bbox[1] > page_height * IGNORE_IMAGE_THRESHOLD) or (img_bbox[1] <= HEADER_FOOTER_THRESHOLD): 
-                    cprint.yellow(f"ignored image {img_filename} bbox: {img_bbox} ")
-                    ignored = True
-                    # continue
+                    cprint.black(f"ignored image {img_filename} bbox: {img_bbox} ")
+                    ignored_images.add(img_bbox)
+                    continue
 
                 # if image is an xref
                 if isinstance(img_data, int):
@@ -110,19 +109,15 @@ def extract_pdf(doc: pymupdf.Document,start=0, end=-1):
                         continue
                     
                     pix = extract_img_from_xref(doc, xref)
-                    if not ignored: images[img_filename] = pix.tobytes()
-                    else: 
-                        ignored_images[img_filename] = hashlib.md5(pix.tobytes()).hexdigest()
+                    images[img_filename] = pix.tobytes()
                     pix = None
                 elif isinstance(img_data, bytes): # otherwise it's the byte data
-                    if not ignored: images[img_filename] = img_data
-                    else: ignored_images[img_filename] = hashlib.md5(img_data).hexdigest()
+                    images[img_filename] = img_data
                 else:
                     cprint.red(f"Image not recognized! Img data: \n{img_data}")
                     continue
 
-                if not ignored:
-                    content += f'<img src="images/{img_filename}" alt="Image {img_count} on page {i+1}" />\n'
+                content += f'<img src="images/{img_filename}" alt="Image {img_count} on page {i+1}" />\n'
             else:
                 cprint.red(f"Error: unrecognized block type {element["type"]}")
 
@@ -130,14 +125,6 @@ def extract_pdf(doc: pymupdf.Document,start=0, end=-1):
         if img_count < get_images_count:
             cprint.red(f"Missed {get_images_count - img_count} images. Extracting and checking duplicates...")
 
-            rev_ignored_images = {}
-            for key, value in ignored_images.items():
-                rev_ignored_images.setdefault(value, set()).add(key)
-            
-            rev_images = {}
-            for key, value in images.items():
-                rev_images.setdefault(hashlib.md5(value).hexdigest(), set()).add(key)
-            
             for index, img in enumerate(img_list):
                 xref = img[0]
                 image_rects = page.get_image_rects(xref)[0] # this returns (x0, y0, x1, y1)
@@ -154,25 +141,11 @@ def extract_pdf(doc: pymupdf.Document,start=0, end=-1):
                 full_img_bytes = pix.tobytes()
 
                 # check duplicates
-                img_hash = hashlib.md5(full_img_bytes).hexdigest()
-
-                if img_hash in rev_ignored_images or img_hash in rev_images:
-                    #! PROBLEM: get_images hash and get_text("dicts") hash is different.
-                    # use with tomodare (has 2 square images at the start of each page, in header) 
-                    # still ignores said images because it detects those from the first page 
-                    # and gets stored in images. not from ignored_images. 
-                    # maybe this is fine maybe not. 
-                    # perhaps move ignored_images scope to be per page instead of per document
-                    # idk how to compare them now tbh
-                    # ? look docs get_image_info
-                    # maybe use a comparing algo but that'll probably be too slow
-                    # * maybe store as an object or something and compare the bbox or size, and the position
-
-                    duplicate_filename = rev_ignored_images[img_hash] if img_hash in rev_ignored_images else rev_images[img_hash]
-                    # ? maybe change to add duplicate image but using the duplicate_filename instead of full_img_filename to save space?
-                    cprint.black(f"Detected already ignored image: {full_img_filename} duplicate with {duplicate_filename}")
+                if (image_rects in ignored_images):
+                    cprint.black("Ignoring duplicate image.")
                     continue
                 
+                cprint.cyan(f"Adding missing image: {full_img_filename}")
                 images[full_img_filename] = full_img_bytes
                 content += f'<img src="images/{full_img_filename}" alt="Full Image {index} on page {i+1}" />\n'
 
